72d2d97335801af7f1a27da87b17228d6d377298
Xptxhx <1257242140@qq.com>
2023-02-10 17:43:11 +0800

    build

---
+++ cms/node_modules/@strapi/icons/dist/SearchIcon.d.ts
@@ -0,0 +1,3 @@
+import * as React from 'react';
+declare function SvgSearchIcon(props: React.SVGProps<SVGSVGElement>): JSX.Element;
+export default SvgSearchIcon;
---
+++ cms/node_modules/@strapi/icons/dist/Store.d.ts
@@ -0,0 +1,3 @@
+import * as React from 'react';
+declare function SvgStore(props: React.SVGProps<SVGSVGElement>): JSX.Element;
+export default SvgStore;
